92e47c5e7add336e80119c742eaa135b52eaa5f9
olaborde <oml007@live.com>
2018-10-24 20:44:51 -0400

    add label for lastname

--- form.py
+++ form.py
@@ -8,8 +8,9 @@ from wtforms.fields.html5 import EmailField
 
 class signupForm(Form):
 
-    user_name = StringField('user_name', validators=[DataRequired("Please enter a username")])
+    firstname = StringField('firstname', validators=[DataRequired("Please enter a username")])
+    lastname = StringField('lastname', validators=[DataRequired("Please enter a username")])
     password = PasswordField('password', validators=[DataRequired(), Length(min=3, message="password should be between 3 and 20 characteres"), validators.EqualTo('vpassword', message='password mnust match')])
-    vpassword = PasswordField('vpassword', validators=[DataRequired(), Length(min=3, message="password should be between 3 and 20 characteres")])
+    vpassword = PasswordField('verify password', validators=[DataRequired(), Length(min=3, message="password should be between 3 and 20 characteres")])
     email = EmailField('email', validators=[EmailField])
     submit = SubmitField('signup')
