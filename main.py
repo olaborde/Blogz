c6c23260c442395f06329cd68ff755d71271a38f
olaborde <oml007@live.com>
2018-10-24 20:45:44 -0400

    added signup route

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-from werkzeuq import generate_password_hash, check_password_hash
+from werkzeug import generate_password_hash, check_password_hash
 from datetime import datetime
 from form import signupForm
 
@@ -93,7 +93,43 @@ def blog(blog_id):
     blog = Blog.query.filter_by(id=blog_id).one()
     date_posted =blog.date_posted.strftime('%B %d, %Y')
     return render_template('blog.html', blog=blog, date_posted=date_posted)
+
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+  error = None
+  # password = request.form.get['password']
+  # vpassword = request.form.get['vpassword']
+ 
+  form = signupForm() 
   
+  if request.method == 'POST':
+      if form.validate == False:
+        return render_template("user-signup.html", form=form)
+      else:
+            if request.form['password'] != request.form['vpassword']:
+                  # error = 'Password must match'
+                  flash('Passwords must match!')
+                  return redirect(url_for('signup'))
+            elif len(request.form['password']) < 3 or len(request.form['password']) > 20:
+                flash('password must between 3 or 20 characters!')
+                return redirect(url_for('signup'))
+            elif ' ' in (request.form['password']).strip() == True:
+                flash('No spaces allowed!')
+                return redirect(url_for('signup'))
+            elif ' ' in (request.form['user_name']).strip() == True:
+                flash('No spaces allowed in username!')
+                return redirect(url_for('signup'))
+            elif len(request.form['user_name']) < 3 or len(request.form['user_name']) > 20:
+                flash('username must between 3 or 20 characters!')
+                return redirect(url_for('signup')) 
+            else:    
+              return 'Welcome, ' + request.form['user_name']    
+  elif request.method == 'GET':   
+    return render_template("user-signup.html", form=form, error=error)
+
+
+
 
 if __name__ == '__main__':
     app.run()
