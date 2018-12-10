b2f844248540b5210b0112841db84a06344d49f6
olaborde <oml007@live.com>
2018-12-10 04:01:06 -0500

    Updated RDS endpoint

--- main/__init__.py
+++ main/__init__.py
@@ -9,7 +9,8 @@ from werkzeug import generate_password_hash, check_password_hash
 app = Flask(__name__, static_url_path='/static')
 # app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://oz509:FlaskBlog509@blogz305.cdcva0qeex0t.us-east-2.rds.amazonaws.com:3306/blogz305'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['QLALCHEMY_TRACK_MODIFICATIONS'] = True
 # app.config['WHOOSH_BASE'] = 'whoosh'
